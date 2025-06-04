dd5a668e4ba682869118d22885d5777d018431e5
Tousin <214836620+Tousin999@users.noreply.github.com>
2025-06-04 19:57:52 +0530

    set class and objects to userinput

--- script/script.js
+++ script/script.js
@@ -1,13 +1,26 @@
 //to-list app
 
 const todoData = [];
+class UserTodo{
+    constructor(todo, date){
+        this.todo = todo;
+        this.date = date;
+    }
+}
 
 //let user enter there data
 const userInput = document.querySelector('.js-to-do-text');
+const userDate = document.querySelector('.js-user-date')
 const submitBtn = document.querySelector('.js-submit-btn');
 //get the data and store it
 submitBtn.addEventListener('click',()=>{
-    todoData.push(userInput.value);
+    /* const userData = {
+        todo: userInput.value,
+        date: userSetDate.value
+    } */
+   const userData = new UserTodo(userInput.value, userDate.value);
+
+    todoData.push(userData);
     console.log(todoData)
 })
 
