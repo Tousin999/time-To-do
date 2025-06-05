10c02d47fb5ffecf2ac51cf8e2a756a971064e86
Tousin <214836620+Tousin999@users.noreply.github.com>
2025-06-05 17:07:46 +0530

    user input validation check & event delegation

--- script/script.js
+++ script/script.js
@@ -1,10 +1,14 @@
 //to-list app
+// TO-DO error-handling with localstorage
 let todoData = JSON.parse(localStorage.getItem('todoData')) || [];
+
+
 class UserTodo{
     constructor(todo, date){
         this.todo = todo;
         this.date = date;
         this.id = Date.now();
+        this.isComplete = false;
     }
 }
 
@@ -17,16 +21,26 @@ const userDataOutput = document.querySelector('.js-user-data-output');
 renderTodo();
 //get the data and store it
 submitBtn.addEventListener('click',()=>{
-   const userTodo = new UserTodo(userText.value, userDate.value);
+    if(!userText.value){
+        window.alert("no to-do, please enter a to-do");
+        return;
+    }
+    if(!userDate.value){
+        window.alert("enter a date value");
+        return;
+    }
+    const userTodo = new UserTodo(userText.value, userDate.value);
     todoData.push(userTodo);
-    userText.value = ''
+    userText.value = '';
+    userDate.value = '';
     renderTodo();
 })
 
 function renderTodo(){
     let html = ''
     todoData.forEach((data)=>{
-        html += `<p>Todo: ${data.todo}, Date: ${data.date}</p><button class="js-delete-btn" data-id="${data.id}">Delete</button>`
+        //TO-DO add function to checkbox
+        html += `<p>Todo: ${data.todo}, Date: ${data.date}</p><button class="js-delete-btn btn btn2" data-id="${data.id}">Delete</button><input type="checkbox" >`
     })
     userDataOutput.innerHTML = html;
     attachDeleteListener();
@@ -34,13 +48,16 @@ function renderTodo(){
 }
 
 function attachDeleteListener(){
-    document.querySelectorAll('.js-delete-btn').forEach((btn)=>{
-        btn.addEventListener('click',()=>{
-            const deleteId = btn.dataset.id;
+    // Event Delegation
+    userDataOutput.addEventListener('click',(event)=>{
+           if(event.target.classList.contains('js-delete-btn')){
+            const deleteId = event.target.dataset.id;
             todoData = todoData.filter((userTodo)=> userTodo.id != deleteId);
             renderTodo();
+           }
+           //TO-DO modify tododata base on checkBox
         })
-    })
-}
+    }
+
 
 
